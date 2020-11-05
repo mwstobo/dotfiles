937799ea59417d18f2f9f62d5ee82361799f6169
Matt Stobo <mwstobo@gmail.com>
2020-11-04 20:01:04 -0500

    More restructring of init.el

--- init.el
+++ init.el
@@ -2,32 +2,48 @@
 ;;; Commentary:
 ;;; Code:
 
-;;; Basic Configuration
+;;; Constants
+(defconst enabled-repos
+  '(("melpa" . "https://melpa.org/packages/")))
 
-;; package managment
-(require 'package)
-(add-to-list
- 'package-archives
- '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+;;; Utility functions
+(defun ensure-package (package)
+  "Ensure PACKAGE is installed."
+  (when (not (package-installed-p package))
+    (package-refresh-contents)
+    (package-install package)))
 
-(when (not (package-installed-p 'use-package))
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(require 'use-package-ensure)
-(setq use-package-always-ensure t)
-
-;; indentation
-(define-key
-  global-map
-  (kbd "RET")
-  'newline-and-indent)
-
-;; simple
+;;; Configuration from the simple package
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-;; ido
+;;; Visual configuration
+(tool-bar-mode 0)
+(load-theme 'wombat)
+(set-frame-font "Source Code Pro-13")
+(setq inhibit-startup-screen t)
+
+;;; Performance tuning
+(setq gc-cons-threshold 100000000)           ; Performance tuning
+(setq read-process-output-max (* 1024 1024)) ; Performance tuning
+
+;;; Setting up package and use-package
+(require 'package)
+(dolist (repo enabled-repos) (add-to-list 'package-archives repo t))
+(package-initialize)
+
+(ensure-package 'use-package)
+(require 'use-package)
+(setq use-package-compute-statistics t)
+
+(use-package use-package-ensure
+  :config
+  (setq use-package-always-ensure t))
+
+;;; Setting up custom
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+;;; Built-in Emacs packages
 (use-package ido
   :commands ido-everywhere
   :init
@@ -36,65 +52,46 @@
   (ido-everywhere)
   (setq ido-enable-flex-matching t))
 
-;; paren
 (use-package paren
   :config
   (show-paren-mode)
   (setq show-paren-style 'mixed))
 
-;; electric pair
 (use-package elec-pair
   :config
   (electric-pair-mode))
 
-;; compile
 (use-package compile
   :config
   (setq compilation-scroll-output 'first-error))
 
-;; set custom file
-(setq custom-file "~/.emacs.d/custom.el")
-(load custom-file)
-
-;; one offs
-(tool-bar-mode 0)                            ; Turn off the tool bar
-(load-theme 'wombat)                         ; Use the wombat theme
-(set-frame-font "Source Code Pro-11")        ; Set the font
-(setq inhibit-startup-screen t)              ; Get rid of the startup screen
-(setq gc-cons-threshold 100000000)           ; Performance tuning
-(setq tab-width 4)                           ; Tabs display with a width of 4
-(setq read-process-output-max (* 1024 1024)) ; Performance tuning
-
 (use-package cc-mode
-  :defines
-  c-basic-offset
   :config
-  (add-hook 'java-mode-hook 'configure-java-indentation))
-(defun configure-java-indentation ()
-  "Configure indentation style for Java code."
-  (setq c-basic-offset 4)
-  (setq tab-width 4)
-  (setq indent-tabs-mode t))
+  (add-hook
+   'java-mode-hook
+   (lambda ()
+     (setq c-basic-offset 4)
+     (setq tab-width 4)
+     (setq indent-tabs-mode t))))
 
-;; line numbers
+(use-package js
+  :config
+  (add-hook
+   'js-mode-hook
+   (lambda ()
+     (setq js-indent-level 2))))
+
 (use-package display-line-numbers
   :config
   (global-display-line-numbers-mode))
 
-;; org mode
 (use-package org
   :config
   (setq org-todo-keywords
-	'((sequence "TODO(t)" "IN-PROGRESS(p)" "NEEDS-DEPLOY(y)"
-		    "|" "IN-REVIEW(r)" "ON-HOLD(h)" "DONE(d)"))))
+	'("TODO(t)" "IN-PROGRESS(p)" "NEEDS-DEPLOY(y)"
+	  "|" "IN-REVIEW(r)" "ON-HOLD(h)" "DONE(d)")))
 
 ;;; Installed major modes
-(use-package js
-  :config
-  (add-hook
-   'js-mode-hook (lambda ()
-		   (setq js-indent-level 2))))
-
 (use-package go-mode
   :mode "\\.go\\'")
 
@@ -111,14 +108,13 @@
   :mode "docker-compose[^/]*\\.ya?ml\\'")
 
 (use-package terraform-mode
-  :commands terraform-format-on-save-mode
   :mode "\\.tf\\(vars\\)?\\'")
 
 (use-package scala-mode
-  :mode "\\.\\(scala\\|sbt\\|worksheet\\.sc\\)\\'"
-  :interpreter "scala")
+  :mode "\\.\\(scala\\|sbt\\|worksheet\\.sc\\)\\'")
 
 (use-package sbt-mode
+  :after scala-mode
   :commands sbt-start sbt-command
   :config
   ;; WORKAROUND: allows using SPACE when in the minibuffer
@@ -139,7 +135,8 @@
 (use-package graphql-mode
   :mode "\\.\\(graphql\\|gql\\)\\'")
 
-(use-package sql-indent)
+(use-package sql-indent
+  :mode "\\.sql\\")
 
 ;;; Other useful packages
 (use-package which-key
