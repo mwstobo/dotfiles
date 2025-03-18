ee09735e7685dd017414b2e7a4a7f4e4f3f90315
Matt Stobo <matt.stobo@cockroachlabs.com>
2025-03-18 11:03:58 -0400

    Extra avy commands

--- init.el
+++ init.el
@@ -126,9 +126,42 @@
 (use-package rg
   :ensure t)
 
+(use-package ring
+  :commands ring-ref)
+
 (use-package avy
   :ensure t
-  :bind ("M-n" . avy-goto-char-timer))
+  :commands avy-action-copy-whole-line
+  :bind ("M-j" . avy-goto-char-timer)
+  :init
+  (defun avy-action-kill-whole-line (pt)
+    (save-excursion
+      (goto-char pt)
+      (kill-whole-line))
+    (select-window
+     (cdr
+      (ring-ref avy-ring 0)))
+    t)
+  (setf (alist-get ?k avy-dispatch-alist) 'avy-action-kill-stay
+        (alist-get ?K avy-dispatch-alist) 'avy-action-kill-whole-line)
+  (defun avy-action-copy-whole-line (pt)
+    (save-excursion
+      (goto-char pt)
+      (cl-destructuring-bind (start . end)
+          (bounds-of-thing-at-point 'line)
+        (copy-region-as-kill start end)))
+    (select-window
+     (cdr
+      (ring-ref avy-ring 0)))
+    t)
+  (setf (alist-get ?w avy-dispatch-alist) 'avy-action-copy
+        (alist-get ?W avy-dispatch-alist) 'avy-action-copy-whole-line)
+  (defun avy-action-yank-whole-line (pt)
+    (avy-action-copy-whole-line pt)
+    (save-excursion (yank))
+    t)
+  (setf (alist-get ?y avy-dispatch-alist) 'avy-action-yank
+        (alist-get ?Y avy-dispatch-alist) 'avy-action-yank-whole-line))
 
 (use-package consult
   :ensure t
